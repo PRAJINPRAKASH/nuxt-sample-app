6c35cef9c766eff7c8a19acb3a1009933c97cc8f
PRAJIN PRAKASH <prajin@techentsia.com>
2020-04-27 11:13:33 +0530

    split index page into script and template

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,45 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-row justify="center" no-gutters>
+    <v-col cols="12">
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+        <logo /></div
+    ></v-col>
+    <v-col cols="12" sm="10" md="8">
+      <v-skeleton-loader
+        transition="scale-transition"
+        :loading="loading"
+        :types="{ actions: 'button@1', skeleton: 'article, actions' }"
+        type="skeleton"
+        width="auto"
+      >
+        <v-card>
+          <v-card-title class="headline">
+            Welcome to To do app
+          </v-card-title>
+          <v-card-text>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum, nisi? Voluptatum possimus repellendus, iste quas
+              corrupti ducimus fugit alias? Saepe impedit cupiditate facilis
+              explicabo rerum vitae quasi recusandae modi tenetur?
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+              voluptatem accusamus veritatis, quo sequi molestias, dolor velit
+              eius excepturi facere rerum impedit magni laudantium minus ducimus
+              amet delectus reprehenderit facilis.
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary" nuxt to="/tests">
+              Continue <v-icon dark left>mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-skeleton-loader>
+    </v-col>
+  </v-row>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
-</script>
+<script src="./index.js"></script>
